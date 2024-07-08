fa271a7b486ed9f12fcaeae56f49a71c022f8dea
Brian Pile <brian.pile@gmail.com>
2024-07-08 11:27:50 -0400

    created extract_eml_dpdv_max function

---
+++ R/extract_eml_dpdv_max.R
@@ -0,0 +1,15 @@
+#' Extract the max dP/dV from the EAM P-Vea curve
+#'
+#' @param V a numeric vector of sourced EAM voltages
+#' @param P a numeric vector of measured EML output powers
+#'
+#' @return a numeric value
+#' @export
+#'
+#' @examples
+extract_eml_dpdv_max = function(V, P) {
+  dPdV = my_derivative(V, P)
+  Vop = extract_eml_vop(V, dPdV)
+  dPdV_max = stats::approx(V, dPdV, Vop)$y
+  return(dPdV_max)
+}
--- R/extract_eml_pf.R
+++ R/extract_eml_pf.R
@@ -1,7 +1,7 @@
 #' Extract Pf from EML P-Vea data
 #'
 #' @param voltage A numeric vector of source voltages
-#' @param power A nueric vector of measured optical powers
+#' @param power A numeric vector of measured optical powers
 #' @param V0 A numeric value of modulator bias voltage
 #'
 #' @return A numeric value for Pf
