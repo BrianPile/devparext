9dd9c17c8d27c469dcaaaada59dbe9318f2dc10f
Brian Pile <brian.pile@gmail.com>
2024-07-08 19:05:59 -0400

    working on kink algorithm

--- my_helper_development_scripts/kink_tester.R
+++ my_helper_development_scripts/kink_tester.R
@@ -10,13 +10,6 @@ graphical_debug_kink = function(I, P, group, ...) {
   # invisible(readline(prompt = "press [enter] to continue: "))
 }
 
-# csv_path = system.file(
-#   "extdata", package = "devparext", mustWork = TRUE
-# ) |> list.files(
-#   full.names = TRUE,
-#   pattern = "stelight_noisy_liv_data.csv"
-# )
-
 #### load some data ####
 
 # HTOL28 problem data
