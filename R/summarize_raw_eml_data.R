a4ad9ef3a755bb3ee770aa46894ba8d6fcb183ba
Brian Pile <brian.pile@gmail.com>
2024-07-08 18:42:33 -0400

    fix bug in summarize_raw_eml_data

--- R/summarize_raw_eml_data.R
+++ R/summarize_raw_eml_data.R
@@ -11,7 +11,7 @@ summarize_raw_eml_data = function(df_eml) {
   df_summary_eml = df_eml |>
     dplyr::summarize(
       Pf1ea0 = extract_eml_pf(.data$voltage, .data$power),
-      Veaop = extract_eml_vop(.data$voltage, .data$dVdP),
+      Veaop = extract_eml_vop(.data$voltage, .data$dPdV),
       Pfop = extract_eml_pf(.data$voltage, .data$power, V0 = 0),
       DCERop = extract_eml_dcer(.data$power, .data$voltage, V0 = 0, V1 = .data$Veaop),
       dPdVmax = extract_eml_dpdv_max(.data$voltage, .data$dPdV),
