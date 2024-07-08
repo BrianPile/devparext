07439f9bf28185a43f4115c9868458110f25e72a
Brian Pile <brian.pile@gmail.com>
2024-07-08 11:40:16 -0400

    created summarize_raw_eml_data function

---
+++ R/summarize_raw_eml_data.R
@@ -0,0 +1,22 @@
+#' Create a summary from the raw data
+#'
+#' @param df_eml a data frame
+#'
+#' @return a data frame
+#' @importFrom rlang .data
+#' @export
+#'
+#' @examples
+summarize_raw_eml_data = function(df_eml) {
+  df_summary_eml = df_eml |>
+    dplyr::summarize(
+      Pf1ea0 = extract_eml_pf(.data$voltage, .data$power),
+      Veaop = extract_eml_vop(.data$voltage, .data$dVdP),
+      Pfop = extract_eml_pf(.data$voltage, .data$power, V0 = 0),
+      DCERop = extract_eml_dcer(.data$power, .data$voltage, V0 = 0, V1 = .data$Veaop),
+      dPdVmax = extract_eml_dpdv_max(.data$voltage, .data$dPdV),
+      Rphoto = extract_eml_rphoto(.data$voltage, .data$dVdI, .data$Veaop)
+    )
+
+  return(df_summary_eml)
+}
