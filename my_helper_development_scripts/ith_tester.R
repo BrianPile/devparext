5518223fa8c9da6e86f1d7048748fc6952d32572
Brian Pile <brian.pile@gmail.com>
2024-07-17 13:13:40 -0400

    doing some ith testing

--- my_helper_development_scripts/ith_tester.R
+++ my_helper_development_scripts/ith_tester.R
@@ -3,6 +3,7 @@ library(tidyverse)
 # plots view debug analysis function
 graphical_debug_ith = function(I, P, group, ...) {
   print(group)
+  # print(group$SN)
 
   Ith_list = extract_ith_from_pi(I, P, ...)
   print(Ith_list)
@@ -23,18 +24,23 @@ graphical_debug_ith = function(I, P, group, ...) {
 # HTOL28 problem data
 df_liv = read_csv(file = "./inst/extdata/HTOL28 problem liv/71634_10-23-54-89.csv")
 
+# WA24-0013 liv
+df_liv = load_raw_liv_data_files(paths = "/Users/brianpile/POET Technologies Dropbox/Brian Pile/Allentown Lab/Work Orders/WA24-0013/LIV_POETA") |>
+  select(-test_date_liv)
+
 # walk through the groups and view the ith algorithm internal data
 # variables
-n1_smooth = 2
-n2_smooth = 9
-n3_smooth = 9
+n1_smooth = 1
+n2_smooth = 3
+n3_smooth = 3
 
 df_liv |>
   # filter(SN %in% sn_Ith1d_probs) |>
-  # filter(SN == "71636_30-41-17-H6") |>
-  group_by(SN) |>
+  filter(SN == "24FC00396-T13-NA-NA") |>
+  # group_by(SN) |>
+  group_by(across(-c(current:dVdI))) |>
   group_walk( ~ graphical_debug_ith(
-    .x$current, .x$pd_current, .y,
+    .x$current, .x$power, .y,
     n1_smooth, n2_smooth, n3_smooth,
     plot_debug = TRUE))
 
