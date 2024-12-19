363a70aac71c008e1b09780fcc427759959b6cb7
Brian Pile <brian.pile@gmail.com>
2024-12-19 10:53:47 -0500

    fix: potential error in linear fit when there are less than two data points

--- R/my_linear_fit.R
+++ R/my_linear_fit.R
@@ -9,6 +9,18 @@
 #' @examples
 my_linear_fit = function(x, y) {
   linear_model = stats::lm(y ~ x)
+
+  if (nrow(stats::coefficients(summary(linear_model))) < 2) {
+    warning("Problem in my_linear_fit(). Returning NA.")
+    return(
+      data.frame(
+        m = NA,
+        b = NA,
+        r2 = NA
+      )
+    )
+  }
+
   m = stats::coef(summary(linear_model))["x", "Estimate"]
   b = stats::coef(summary(linear_model))["(Intercept)", "Estimate"]
   r2 = summary(linear_model)$r.squared
