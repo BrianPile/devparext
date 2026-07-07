d0895e1ea2f9accd9a81ae6ad5aed9daf1c94b75
Brian Pile <brian.pile@gmail.com>
2026-07-07 16:30:14 +0800

    Update: handle single-valued x and y. Also detect edge case where x has zero variance.

--- R/my_linear_fit.R
+++ R/my_linear_fit.R
@@ -18,6 +18,7 @@ my_linear_fit = function(x, y) {
     r2 = NA_real_
   )
 
+  # input validation
   if (!is.numeric(x) || !is.numeric(y)) {
     stop("`x` and `y` must be numeric vectors.")
   }
@@ -26,38 +27,28 @@ my_linear_fit = function(x, y) {
     stop("`x` and `y` must have the same length.")
   }
 
+  if (length(x) == 1) {
+    warning("`x` and `y` are single-valued. A linear fit required at aleast two points. Returning NA.")
+    return(na_result)
+  }
+
   if (anyNA(x) || anyNA(y)) {
     warning("`x` and `y` must not contain missing values. Returing NA.")
     return(na_result)
   }
 
+  # model fitting
   linear_model = stats::lm(y ~ x)
   fit_summary = summary(linear_model)
   coefs = stats::coef(fit_summary)
 
-  # if (nrow(stats::coef(summary(linear_model))) < 2) {
-  #   warning("Problem in my_linear_fit(). Returning NA.")
-  #   return(
-  #     data.frame(
-  #       m = NA_real_,
-  #       b = NA_real_,
-  #       r2 = NA_real_
-  #     )
-  #   )
-  # }
+  # handle other edge cases (e.g., x = c(2, 2, 2) where length > 1 but variance is 0)
+  if (!"x" %in% rownames(coefs)) {
+    warning("Slope could not be estimated (possibly due to zero variance in x). Returning NA.")
+    return(na_result)
+  }
 
-  # m = stats::coef(summary(linear_model))["x", "Estimate"]
-  # b = stats::coef(summary(linear_model))["(Intercept)", "Estimate"]
-  # r2 = summary(linear_model)$r.squared
-  #
-  # return(
-  #   data.frame(
-  #     m = m,
-  #     b = b,
-  #     r2 = r2
-  #   )
-  # )
-
+  # extract and return results
   data.frame(
     m = unname(coefs["x", "Estimate"]),
     b = unname(coefs["(Intercept)", "Estimate"]),
